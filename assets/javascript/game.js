78b170127bdc8ef3f72a41db71cc3a7d204d1caf
Josh Latour <jlatour88@gmail.com>
2019-05-13 14:26:23 -0500

    added console logs and html inserts

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,14 +4,17 @@ var wins = 0;
 var losses = 0;
 var targetNumber = 0;
 var crystalNumbers = [];
-var crystal1 = crystalNumbers[0]
-var crystal2 = crystalNumbers[1]
-var crystal3 = crystalNumbers[2]
-var crystal4 = crystalNumbers[3]
+var crystal1;
+var crystal2;
+var crystal3;
+var crystal4;
 
-console.log('targetNumber: ', targetNumber);
-console.log('crystalNumbers: ', crystalNumbers);
-
+console.log('Global targetNumber: ', targetNumber);
+console.log('Global crystalNumbers: ', crystalNumbers);
+console.log('crystal1: ', crystal1);
+console.log('crystal2: ', crystal2);
+console.log('crystal3: ', crystal3);
+console.log('crystal4: ', crystal4);
 
 
 
@@ -21,14 +24,19 @@ function newRound() {
 crystalNumbers = [];
    // Create for loop to output 4 random numbers
    for (var i = 0; i < 4; i++) {
-      crystalNumbers.push(Math.floor(Math.random() * 12) + 1)
+      crystalNumbers.push(Math.floor(Math.random() * 12) + 1);
    }
    // Find a new target number
 targetNumber = Math.floor(Math.random() * 102) + 19;
 
+$("#target-number").html(targetNumber);
+$("#crystal1").html(crystal1);
+$("#crystal2").html(crystal2);
+$("#crystal3").html(crystal3);
+$("#crystal4").html(crystal4);
+
 // Console Logs
+console.log('function/targetNumber: ', targetNumber);
 console.log('function/crystalNumbers: ', crystalNumbers);
 }
 newRound();
-
-// maybe need to have the loop in a function of its own to be called when needed.
