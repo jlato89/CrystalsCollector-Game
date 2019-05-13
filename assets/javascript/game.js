181aba459114f80e2df2cbce6c44be16675e481d
Josh Latour <jlatour88@gmail.com>
2019-05-13 11:42:01 -0500

    changed some names and working on new function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,28 +2,33 @@
 var userScore = 0;
 var wins = 0;
 var losses = 0;
-var computerNumber = Math.floor(Math.random() * 102) + 19;
-var crystalNumbers = [1, 2, 3, 4];
+var targetNumber = 0;
+var crystalNumbers = [];
 var crystal1 = crystalNumbers[0]
 var crystal2 = crystalNumbers[1]
 var crystal3 = crystalNumbers[2]
 var crystal4 = crystalNumbers[3]
 
-
+console.log('targetNumber: ', targetNumber);
 console.log('crystalNumbers: ', crystalNumbers);
 
 
 
 
 // *FUNCTIONS*
-function nextRound() {
-   // Random number generator
-computerNumber = Math.floor(Math.random() * 102) + 19;
+function newRound() {
+   // Reset to 0
 crystalNumbers = [];
-console.log('Function/crystalNumbers: ', crystalNumbers);
    // Create for loop to output 4 random numbers
    for (var i = 0; i < 4; i++) {
       crystalNumbers.push(Math.floor(Math.random() * 12) + 1)
    }
+   // Find a new target number
+targetNumber = Math.floor(Math.random() * 102) + 19;
+
+// Console Logs
+console.log('function/crystalNumbers: ', crystalNumbers);
 }
-nextRound();
+newRound();
+
+// maybe need to have the loop in a function of its own to be called when needed.
